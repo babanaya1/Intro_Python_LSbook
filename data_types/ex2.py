bde6eae079195a68dc35b61aac36c22f591850d1
babanaya1 <nuhuhamidu1989@gmail.com>
2025-06-06 06:55:21 +0000

    Finish data types exercises

---
+++ data_types/ex2.py
@@ -0,0 +1,11 @@
+# Pet names tuple.
+names = (
+    'Asta',
+    'Buttescotch',
+    'Pudding',
+    'Neptune',
+    'Darwin',
+)
+
+print(type(names))
+print(names)
---
+++ data_types/ex3.py
@@ -0,0 +1,12 @@
+# Pet names and type dictionary.
+
+pets = {
+    'Asta': 'dog',
+    'Butterscotch': 'cat',
+    'Pudding': 'cat',
+    'Neptune': 'fish',
+    'Darwin': 'lizard',
+}
+
+print(type(pets))
+print(pets)
